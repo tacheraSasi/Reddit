a971872ee1356c1b5348b23406b40236cda3a6dd
Lukas Grinevicius <lukasgrin8@gmail.com>
2025-02-21 18:04:42 +0200

    Home Screen Finished

---
+++ src/app/(tabs)/communities.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CommunitiesScreen() {
+  return (
+    <View>
+      <Text>Communities Screen</Text>
+    </View>
+  )
+}
---
+++ src/app/(tabs)/create.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CreateScreen() {
+  return (
+    <View>
+      <Text>Create Screen</Text>
+    </View>
+  )
+}
