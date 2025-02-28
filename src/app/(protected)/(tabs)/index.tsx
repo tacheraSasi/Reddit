abcf23ca1995149b96fe93c6bcc57af806b9f90f
Vadim Savin <savinvadim1312@gmail.com>
2025-02-28 16:32:51 +0100

    setup tanstack query

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,31 +1,44 @@
 import { useState, useEffect } from "react";
-import { View, FlatList } from "react-native";
+import { View, FlatList, ActivityIndicator, Text } from "react-native";
 import PostListItem from "../../../components/PostListItem";
 import { supabase } from "../../../lib/supabase";
 import { Tables } from "../../../types/database.types";
+import { useQuery } from "@tanstack/react-query";
 
 type Post = Tables<"posts"> & {
   user: Tables<"users">;
   group: Tables<"groups">;
 };
 
+const fetchPosts = async () => {
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
+  if (error) {
+    throw error;
+  } else {
+    return data;
+  }
+};
+
 export default function HomeScreen() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts(),
+  });
 
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
 
-  const fetchPosts = async () => {
-    const { data, error } = await supabase
-      .from("posts")
-      .select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
-    if (error) {
-      console.log(error);
-    } else {
-      setPosts(data);
-    }
-  };
+  if (error) {
+    console.log(error);
+    return <Text>Error fetching posts</Text>;
+  }
 
   return (
     <View>
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,27 @@
-import { tokenCache } from '../../cache'
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
-import { Slot } from 'expo-router'
+import { tokenCache } from "../../cache";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { Slot } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  useReactQueryDevTools(queryClient);
 
   if (!publishableKey) {
-    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+    throw new Error("Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env");
   }
 
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
-        <Slot />
-      </ClerkLoaded>
-    </ClerkProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+        <ClerkLoaded>
+          <Slot />
+        </ClerkLoaded>
+      </ClerkProvider>
+    </QueryClientProvider>
+  );
 }
