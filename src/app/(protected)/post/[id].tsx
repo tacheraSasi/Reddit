5b293a1f9d4428fcce387dabe0c6e00fde7544a2
Vadim Savin <savinvadim1312@gmail.com>
2025-03-05 15:51:49 +0100

    Comments UI

--- src/app/(protected)/post/[id].tsx
+++ src/app/(protected)/post/[id].tsx
@@ -1,20 +1,101 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { useLocalSearchParams } from "expo-router";
-import posts from '../../../../assets/data/posts.json'
+import posts from "../../../../assets/data/posts.json";
 import PostListItem from "../../../components/PostListItem";
+import comments from "../../../../assets/data/comments.json";
+import CommentListItem from "../../../components/CommentListItem";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function DetailedPost() {
-  const { id } = useLocalSearchParams()
+  const { id } = useLocalSearchParams();
+  const [comment, setComment] = useState<string>("");
+  const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
 
-  const detailedPost = posts.find((post) => post.id === id)
+  const insets = useSafeAreaInsets();
+
+  const detailedPost = posts.find((post) => post.id === id);
+
+  const postComments = comments.filter(
+    (comment) => comment.post_id === "post-1"
+  );
+
+  console.log(postComments);
 
   if (!detailedPost) {
-    return <Text>Post Not Found</Text>
+    return <Text>Post Not Found</Text>;
   }
 
   return (
-    <View>
-      <PostListItem post={detailedPost} isDetailedPost />
-    </View>
-  )
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={insets.top + 10}
+    >
+      <FlatList
+        data={postComments}
+        renderItem={({ item }) => <CommentListItem comment={item} />}
+        ListHeaderComponent={
+          <PostListItem post={detailedPost} isDetailedPost />
+        }
+      />
+      <View
+        style={{
+          paddingBottom: insets.bottom,
+          borderBottomColor: "lightgray",
+          padding: 10,
+          backgroundColor: "white",
+          borderRadius: 10,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: -3,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 3,
+          elevation: 4,
+        }}
+      >
+        <TextInput
+          placeholder="Join the conversation"
+          style={{ backgroundColor: "#E4E4E4", padding: 5, borderRadius: 5 }}
+          value={comment}
+          onChangeText={(text) => setComment(text)}
+          multiline
+          onFocus={() => setIsInputFocused(true)}
+          onBlur={() => setIsInputFocused(false)}
+        />
+        {isInputFocused && (
+          <Pressable
+            style={{
+              backgroundColor: "#0d469b",
+              borderRadius: 15,
+              marginLeft: "auto",
+              marginTop: 15,
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                paddingVertical: 5,
+                paddingHorizontal: 10,
+                fontWeight: "bold",
+                fontSize: 13,
+              }}
+            >
+              Reply
+            </Text>
+          </Pressable>
+        )}
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
